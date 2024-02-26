1466250a6691e25a2eabfed3999bc61fe65d0627
杨孙航 <13446852+yang-sunhang@user.noreply.gitee.com>
2024-02-26 20:24:16 +0800

    6 commit

--- model/goods.go
+++ model/goods.go
@@ -1,8 +1,32 @@
 package model
 
+import "gorm.io/gorm"
+
 type Goods struct {
-	ID         int
+	gorm.Model
 	GoodsName  string
 	GoodsPrice float64
 	GoodsNum   int
+}
+
+func (g *Goods) Create(goods Goods) (Goods, error) {
+	err := db.Model(g).Create(&goods).Error
+	return goods, err
+}
+
+func (g *Goods) Delete(id int) error {
+	err := db.Model(g).Where("id = ?", id).Delete(g).Error
+	return err
+}
+
+func (g *Goods) Upload(id int) (Goods, error) {
+	var goods Goods
+	err := db.Model(g).Where("id = ?", id).Delete(g).Error
+	return goods, err
+}
+
+func (g *Goods) Get(id int) (Goods, error) {
+	var goods Goods
+	err := db.Model(g).Where("id = ?", id).First(&goods).Error
+	return goods, err
 }
