ab81aebb8491db5e3b2a30bc172b985dc6b9570e
杨孙航 <13446852+yang-sunhang@user.noreply.gitee.com>
2024-03-04 16:21:39 +0800

    8

--- model/init.go
+++ model/init.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 	"log"
 )
 
@@ -14,10 +13,8 @@ var err error
 func InItMysql() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", NaCosT.Username,
 		NaCosT.Password, NaCosT.Host, NaCosT.Port, NaCosT.Mysqlbase)
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
-	err := db.AutoMigrate(new(User))
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	err := db.AutoMigrate(new(Goods))
 	if err != nil {
 		log.Println(err, "**********************AutoMigrate")
 		return
