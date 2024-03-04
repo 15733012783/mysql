d5ba18b48623ba662634176484eb5ad5b9ea756a
杨孙航 <13446852+yang-sunhang@user.noreply.gitee.com>
2024-03-04 09:55:03 +0800

    8

--- model/goods.go
+++ model/goods.go
@@ -9,6 +9,7 @@ type Goods struct {
 	GoodsName  string  `gorm:"index"`
 	GoodsPrice float64 `gorm:"decimal(10,2)"`
 	GoodsNum   int     `gorm:"tinyint"`
+	GoodsPhoto string  `gorm:"varchar(100)"`
 }
 
 func NewGoods() *Goods {
@@ -34,5 +35,13 @@ func (g *Goods) Upload(goods *Goods) (info *Goods, err error) {
 func (g *Goods) Get(goodsName string) (info *Goods, err error) {
 	info = new(Goods)
 	err = db.Model(g).Where("goods_name = ?", goodsName).First(&info).Error
+	return info, err
+}
+
+func (g *Goods) UploadFile(id int, FileName string) (info *Goods, err error) {
+	info = new(Goods)
+	err = db.Model(g).Where("id = ?", id).First(info).Error
+	info.GoodsPhoto = FileName
+	err = db.Model(g).Save(info).Error
 	return info, err
 }
