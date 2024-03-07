a4b4022a4e1f68f8d9ed83d89b22aa8546c07f5d
杨孙航 <13446852+yang-sunhang@user.noreply.gitee.com>
2024-03-07 09:50:56 +0800

    2

--- consul/consul.go
+++ consul/consul.go
@@ -2,7 +2,6 @@ package consul
 
 import (
 	"fmt"
-	"github.com/google/uuid"
 	"github.com/hashicorp/consul/api"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -15,36 +14,24 @@ var ConSuLClient *api.Client
 
 func SonSul() {
 	var err error
+	api.DefaultConfig().Address = fmt.Sprintf("%s:%d", "10.2.171.70", 8500)
 	ConSuLClient, err = api.NewClient(api.DefaultConfig())
 	if err != nil {
-		return
+		zap.S().Panic(err.Error())
 	}
-	api.DefaultConfig().Address = fmt.Sprintf("%s:%d", "10.2.171.70", 8500)
-	err = ConSuLClient.Agent().ServiceRegister(&api.AgentServiceRegistration{
-		ID:      uuid.NewString(),
-		Name:    "test",
-		Tags:    []string{"GRPC"},
-		Port:    8081,
-		Address: "10.2.171.70",
-	})
 
 	check := &api.AgentServiceCheck{
-		GRPC:     fmt.Sprintf("%s:%d", "10.2.171.70", 8081), // 这里一定是外部可以访问的地址
-		Timeout:  "10s",                                     // 超时时间
-		Interval: "10s",                                     // 运行检查的频率
-		// 指定时间后自动注销不健康的服务节点
-		// 最小超时时间为1分钟，收获不健康服务的进程每30秒运行一次，因此触发注销的时间可能略长于配置的超时时间。
+		GRPC:                           fmt.Sprintf("%s:%d", "10.2.171.70", 8081),
+		Timeout:                        "10s",
+		Interval:                       "10s",
 		DeregisterCriticalServiceAfter: "1m",
 	}
 	srv := &api.AgentServiceRegistration{
-		Name:    "test",                    // 服务名称
-		Tags:    []string{"q1mi", "hello"}, // 为服务打标签
+		Name:    "test",
+		Tags:    []string{"q1mi", "hello"},
 		Address: "10.2.171.70",
 		Port:    8081,
 		Check:   check,
-	}
-	if err != nil {
-		zap.S().Panic(err.Error())
 	}
 	err = ConSuLClient.Agent().ServiceRegister(srv)
 	if err != nil {
