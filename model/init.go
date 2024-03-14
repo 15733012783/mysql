b9bcb2ed4a8b0e3b4efab0fd448abf5fe5f405f0
杨孙航 <13446852+yang-sunhang@user.noreply.gitee.com>
2024-03-14 16:54:27 +0800

    first commit

--- model/init.go
+++ model/init.go
@@ -15,7 +15,7 @@ func InItMysql() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", nacos.GoodsT.Mysql.Username,
 		nacos.GoodsT.Mysql.Password, nacos.GoodsT.Mysql.Host, nacos.GoodsT.Mysql.Port, nacos.GoodsT.Mysql.Mysqlbase)
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	err := db.AutoMigrate(new(Goods))
+	err := db.AutoMigrate(new(Goods), new(CGoods), new(Property), new(PropertyValue), new(Sku))
 	if err != nil {
 		log.Println(err, "**********************AutoMigrate")
 		return
